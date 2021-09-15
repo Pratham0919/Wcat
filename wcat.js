01597be2cba2f53557a2a9ed4cd9485aa3ecb654
unknown <verma.pratham09@gmail.com>
2021-09-15 20:30:15 +0530

    first commit

---
+++ wcat.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+let fs=require("fs");
+//input
+let inputArr=process.argv.slice(2);
+//console.log(inputArr);
+//identify->options
+let optionsArr=[];
+let filesArr=[];
+for(let i=0;i<inputArr.length;i++)
+{
+    let firstChar=inputArr[i].charAt(0);
+    if(firstChar=="~")
+    {
+        optionsArr.push(inputArr[i]);
+    }
+    else{
+        filesArr.push(inputArr[i]);
+    }
+}
+
+//option check
+let isBothPresent=optionsArr.includes("~b") && optionsArr.includes("~n");
+if(isBothPresent==true)
+{
+    console.log("either enter ~n or ~b");
+    return;
+}
+//existence
+for(let i=0;i<filesArr.length;i++)
+{
+    let isPresent=fs.existsSync(filesArr[i]);
+    if(isPresent==false)
+    {
+        console.log(filesArr[i]+" is not presented")
+        return;
+    }
+}
+
+//read
+let content="";
+for(let i=0;i<filesArr.length;i++)
+{
+    //buffer
+    let bufferContent=fs.readFileSync(filesArr[i]);
+    content=content+bufferContent+"\r\n";
+}
+//console.log(content);
+let contentArr=content.split("\r\n");
+//console.log(contentArr);
+
+//~s
+let isSPresent=optionsArr.includes("~s");
+if(isSPresent==true)
+{
+    for(let i=1;i<contentArr.length;i++)
+    {
+        if(contentArr[i]=="" && contentArr[i-1]=="")
+        {
+            contentArr[i]=null;
+        }
+        else if(contentArr[i]=="" && contentArr[i-1]==null)
+        {
+            contentArr[i]=null;
+        }
+    }
+    let tempArr=[];
+    for(let i=0;i<contentArr.length;i++)
+    {
+        if(contentArr[i]!=null)
+        {
+            tempArr.push(contentArr[i]);
+        }
+    }
+    contentArr=tempArr;
+}
+console.log("``````````````````");
+//console.log(contentArr.join("\n"));
+//~n
+let isNPresent=optionsArr.includes("~n");
+if(isNPresent==true)
+{
+    for(let i=0;i<contentArr.length;i++)
+    {
+        contentArr[i]=`${i+1} ${contentArr[i]}`;
+    }
+}
+//console.log(contentArr.join("\n"));
+
+//~b
+let isBPresent=optionsArr.includes("~b");
+if(isBPresent==true)
+
+{
+    let count=1;
+    for(let i=0;i<contentArr.length;i++)
+    {
+        if(contentArr[i]!="")
+        {
+            contentArr[i]= `${count} ${contentArr[i]}`;
+            count++;
+        }
+    }
+}
+console.log(contentArr.join("\n"));
+
+
